fc1e2305e318a8f7327dcdefdc27d3babbe059fc
unknown <saeedht2>
2024-12-03 14:15:32 +0500

    porfilo page updated ,add read more detial page

--- js/custom.js
+++ js/custom.js
@@ -369,3 +369,15 @@ $(function () {
 
 
 });
+
+
+  // Optionally, you can add some JavaScript to apply the zoom effect
+  const images = document.querySelectorAll('img');
+  images.forEach(image => {
+	  image.addEventListener('mouseover', () => {
+		  image.style.transform = 'scale(1.5)';
+	  });
+	  image.addEventListener('mouseout', () => {
+		  image.style.transform = 'scale(1)';
+	  });
+  });
